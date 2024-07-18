fdcdc1fdbae1b6f95b1b5640f90c25465ebb69a5
bibarsss <bibaryskoutepov@mail.ru>
2024-07-18 19:05:53 +0500

    add: Search in db

--- app/Http/Controllers/Api/AirportController.php
+++ app/Http/Controllers/Api/AirportController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Airport\SearchRequest;
 use App\Services\Actions\Airport\SearchDBAction;
+use Illuminate\Http\JsonResponse;
 
 class AirportController extends Controller
 {
-    public function searchDB(SearchDBAction $action, SearchRequest $request)
+    public function searchDB(SearchDBAction $action, SearchRequest $request): JsonResponse
     {
-        $data = $action->run();
+        $data = $action->run($request);
         return response()->json(
             [
                 'status' => true,
--- app/Services/Actions/Airport/SearchDBAction.php
+++ app/Services/Actions/Airport/SearchDBAction.php
@@ -2,10 +2,18 @@
 
 namespace App\Services\Actions\Airport;
 
+use App\Models\Airport;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Http\FormRequest;
+
 class SearchDBAction
 {
-    public function run()
+    public function run(FormRequest $request): Collection|array
     {
-        return 'action';
+        $query = $request->input('query');
+        return Airport::query()
+            ->where('city_name_en', 'LIKE', "%{$query}%")
+            ->orWhere('airport_name_en', 'LIKE', "%{$query}%")
+            ->get();
     }
 }
